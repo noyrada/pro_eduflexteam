527799b7c8176ec675b90e8f8e8b0cb4039ec179
noyrada <noyradastudentiq@gmail.com>
2024-10-14 15:24:37 +0700

    create structure of login-form

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// views
+import HomePage from './views/HomePage.jsx';
+//import Login from './views/auth/Login.jsx';
+
+// layouts
+import Auth from './layouts/Auth.jsx';
+
+// Create a root for rendering
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      {/* Route for layouts */}
+      <Route path="/auth/*" element={<Auth />} />
+
+      {/* Route for without layouts */}
+      <Route path="/" element={<HomePage />} />
+
+      {/* Redirect to the home page */}
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  </BrowserRouter>
+);
