7455b8caf7eaf61f8be68352ab8d06032ebe2111
noyrada <noyradastudentiq@gmail.com>
2024-10-17 13:52:05 +0700

    add footer to component

---
+++ src/components/Footer/FooterAdmin.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function FooterAdmin() {
+    return (
+        <>
+            <footer
+                class="flex flex-col items-center bg-white shadow-sm text-center lg:text-left rounded-sm mx-4">
+
+                {/* <!-- Copyright --> */}
+                <div class="w-full p-4 text-center text-gray-500">
+                    © 2024 Copyright:
+                    <a href="https://eduflex.com/"> EduFlex Team </a>
+                </div>
+            </footer>
+        </>
+    )
+}
--- src/views/admin/Dashboard.jsx
+++ src/views/admin/Dashboard.jsx
@@ -10,24 +10,24 @@ import CardLastActivity from "../../components/Cards/CardLastActivity";
 export default function Dashboard() {
   return (
     <>
-      <div className="grid grid-cols-4 gap-4 mx-6 pt-0 h-screen">
+      <div className="grid grid-cols-4 gap-4 mx-8 pt-0 h-screen">
         {/* Card Stats */}
         <div className="col-span-3 bg-white shadow-md h-36">
           <CardStats/>
         </div>
 
         {/* Card Last Activity */}
-        <div className="row-span-2 bg-white shadow-md">
+        <div className="row-span-2 bg-white shadow-sm rounded-sm">
           <CardLastActivity/>
         </div>
 
         {/* Card LineChart */}
-        <div className="col-span-3 bg-white shadow-md h-96">
+        <div className="col-span-3 bg-white shadow-sm rounded-sm h-96">
           <CardLineChart/>
         </div>
 
         {/* Card Courses */}
-        <div className="col-span-4 bg-white shadow-md h-80">
+        <div className="col-span-4 bg-white shadow-sm rounded-sm h-80">
           <CardCourses/>
         </div>
       </div>
