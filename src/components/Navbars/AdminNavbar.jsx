9af32e851b50d394a3ab599d5c840910cc2062e4
noyrada <noyradastudentiq@gmail.com>
2024-10-18 16:47:38 +0700

    merge code borey navbar to main

--- src/components/Navbars/AdminNavbar.jsx
+++ src/components/Navbars/AdminNavbar.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Flag from "../../assets/images/Khmer_language.png";
+import User from "../../assets/images/profile-rada.jpg";
+import { Search, Bell, ChevronDown } from "lucide-react";
 
 // component
 
@@ -6,33 +9,59 @@ export default function Navbar() {
     return (
         <>
             {/* Navbar */}
-            <nav className="top-0 left-0 w-full z-10 bg-gray-200 flex items-center p-2">
+            <nav className="flex top-0 left-0 w-full z-10 bg-blue-600 items-center justify-between p-3 shadow-[0_5px_10px_rgba(0,0,0,0.3)]">
                 <div className="w-full mx-auto items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4">
-                    {/* Brand */}
-                    <a
-                        className="text-white text-sm uppercase hidden lg:inline-block font-semibold"
-                        href="#pablo"
-                        onClick={(e) => e.preventDefault()}
-                    >
-                        Dashboard
-                    </a>
+
                     {/* Form */}
-                    <form className="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3">
-                        <div className="relative flex w-full flex-wrap items-stretch">
+                    <form className="flex-grow flex justify-center">
+                        <div className="relative flex w-full max-w-md">
                             <span className="z-10 h-full leading-snug font-normal text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
                                 <i className="fas fa-search"></i>
                             </span>
                             <input
                                 type="text"
-                                placeholder="Search here..."
-                                className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring w-full pl-10"
+                                placeholder="Search your course..."
+                                className="border-0 px-2 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring w-full pl-10"
                             />
                         </div>
                     </form>
 
                     {/* User */}
-                    <ul className="flex-col md:flex-row list-none items-center hidden md:flex">
+                    <ul className="flex-col md:flex-row list-none items-center hidden md:flex space-x-4">
+                        {/* flag */}
+                        <li>
+                            <button className="cursor-pointer text-white p-0">
+                                <img
+                                    src={Flag}
+                                    alt="flag"
+                                    className="h-7 w-7 rounded-full border"
+                                />
+                            </button>
+                        </li>
+                        {/* Bell icon */}
+                        <li>
+                            <button className="cursor-pointer text-white p-0">
+                                <Bell className="h-7 w-7" />
+                            </button>
+                        </li>
+
+                        {/* User profile */}
+                        <li>
+                            <button className="cursor-pointer text-white p-0">
+                                <img
+                                    src={User}
+                                    alt="flag"
+                                    className="h-7 w-7 rounded-full border"
+                                />
+                            </button>
+                        </li>
+
                         {/* <UserDropdown /> */}
+                        <li>
+                            <button className="cursor-pointer text-white p-1 mr-2">
+                                <ChevronDown className="h-7 w-7" />
+                            </button>
+                        </li>
                     </ul>
                 </div>
             </nav>
