c66fa19fba8591f68e6b5d7848ad43f4500d5fdf
noyrada <noyradastudentiq@gmail.com>
2024-10-18 14:31:09 +0700

    Updated structure of Courses

---
+++ src/components/Cards/Courses/CardEvents.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// components
+
+export default function CardEvents() {
+    return (
+        <>
+            <div>
+                <h1>This is Card Events.</h1>
+            </div>
+        </>
+    )
+}
--- src/views/courses/CourseEvent.jsx
+++ src/views/courses/CourseEvent.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 // components
 import CardToolBar from "../../components/Cards/Courses/CardToolBar";
+import CardEvents from "../../components/Cards/Courses/CardEvents";
 
 export default function CourseAssign() {
     return (
@@ -9,7 +10,7 @@ export default function CourseAssign() {
             <div className="grid grid-rows-[auto,auto,1fr] mx-8">
                 {/* Breadcrumb */}
                 <div className="bg-gray-200 p-4 shadow-sm">
-                    <p>Home &gt; Course</p>
+                    <p>Home &gt; Course &gt; Events</p>
                 </div>
 
                 {/* Tool Bar */}
@@ -20,7 +21,7 @@ export default function CourseAssign() {
                 {/* Content Area with Cards */}
                 <div className="bg-white shadow-md rounded-md mt-2 h-screen">
                     <div className="">
-                        <h1>Courses Events</h1>
+                        <CardEvents/>
                     </div>
                 </div>
             </div>
