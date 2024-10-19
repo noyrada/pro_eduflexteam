ad2cd97e142aa79fb9c46a1365c9867b2d43db64
noyrada <noyradastudentiq@gmail.com>
2024-10-19 10:13:04 +0700

    updated

--- src/views/admin/Dashboard.jsx
+++ src/views/admin/Dashboard.jsx
@@ -13,27 +13,27 @@ export default function Dashboard() {
       <div className="grid grid-cols-4 gap-5 mx-8 pt-0">
         {/* Card Stats */}
         <div className="col-span-3   ">
-        <div class="grid grid-cols-4  gap-4">
-          <CardStats title="Course Completed" icon={<ion-icon name='document-outline'></ion-icon>} num="98" />
-          <CardStats title="In process" icon={<ion-icon name="trending-up-sharp"></ion-icon>} num="98"  />
-          <CardStats title="Upcoming Course" icon={<ion-icon name="document-text-outline"></ion-icon>} num="98"  />
-          <CardStats title="Watch Time"  icon={<ion-icon name='time-outline'></ion-icon>} num="56:25"  />
-        </div>
+          <div class="grid grid-cols-4  gap-4">
+            <CardStats title="Course Completed" icon={<ion-icon name='document-outline'></ion-icon>} num="98" />
+            <CardStats title="In process" icon={<ion-icon name="trending-up-sharp"></ion-icon>} num="98" />
+            <CardStats title="Upcoming Course" icon={<ion-icon name="document-text-outline"></ion-icon>} num="98" />
+            <CardStats title="Watch Time" icon={<ion-icon name='time-outline'></ion-icon>} num="56:25" />
+          </div>
         </div>
 
         {/* Card Last Activity */}
         <div className="row-span-2 bg-white shadow-sm rounded-sm">
-          <CardLastActivity/>
+          <CardLastActivity />
         </div>
 
         {/* Card LineChart */}
         <div className="col-span-3 bg-white shadow-sm rounded-sm h-100">
-          <CardLineChart/>
+          <CardLineChart />
         </div>
 
         {/* Card Courses */}
         <div className="col-span-4 bg-white shadow-sm rounded-sm h-83">
-          <CardCourses/>
+          <CardCourses />
         </div>
       </div>
     </>
