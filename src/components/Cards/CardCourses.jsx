fd369b41fc756e3166e75039875bbc30ac4224f1
noyrada <noyradastudentiq@gmail.com>
2024-10-20 17:08:18 +0700

    updated Card Courses

--- src/components/Cards/CardCourses.jsx
+++ src/components/Cards/CardCourses.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ImageP1 from "../../assets/images/ux.jpg";
-import ImageP2 from "../../assets/images/figma-ios-design-system.jpg";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { MdOutlineSignalCellularAlt } from "react-icons/md";
-<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+import { Link } from "react-router-dom";
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>;
+
 function Image({ image, title, createdDate, level, duration, price }) {
     return (
-        <div class=" border rounded-xl cursor-pointer m-4" >
+        <div class=" border rounded-xl cursor-pointer m-4 marker:shadow-md hover:shadow-xl hover:scale-105 transition duration-300 ease-in-out">
             <img className="rounded-xl h-w-160 w-full" src={image} alt={title} />
             <div className="ml-4">
                 <div className="flex mt-2 ">
@@ -17,11 +19,13 @@ function Image({ image, title, createdDate, level, duration, price }) {
                         <p className="text-gray-600">{price}</p>
                     </div>
                 </div>
-                <div className="mt-5 ">
-                    <h1 className="text-blue-900"><b>{title}</b></h1>
+                <div className="mt-7">
+                    <h1 className="text-blue-900">
+                        <b>{title}</b>
+                    </h1>
                 </div>
-                <div class="mb-2">
-                    <p className="text-gray-600">Created: {createdDate}</p>
+                <div class="mb-2 mt-4">
+                    <p className="text-gray-500 text-base">Created: {createdDate}</p>
                 </div>
             </div>
         </div>
@@ -30,45 +34,56 @@ function Image({ image, title, createdDate, level, duration, price }) {
 
 export default function CardCourses() {
     return (
-        <section >
-            <div className="m-2">
-                <div className="flex mt-2 ml-2 justify-between">
-                    <h7><b>Courses</b></h7>
-                    <div className="flex items-center cursor-pointer">
-                        <p className="text-blue-900">All Courses</p>
-                        <FaArrowRightLong className="ml-2 mr-5 mt-2 text-blue-900" />
-                    </div>
+        <section>
+            <div className="p-2">
+                <div className="flex ml-4 justify-between">
+                    <h7 className="mt-2">
+                        <b>Courses</b>
+                    </h7>
+                    <Link to="/admin/courses">
+                        <div className="flex items-center cursor-pointer">
+                            <p className="text-blue-900 mt-2">All Courses</p>
+                            <FaArrowRightLong className="ml-2 mr-5 mt-2 text-blue-900" />
+                        </div>
+                    </Link>
                 </div>
+
                 {/* image content */}
-                <div class="flex justify-evenly ">
-                    <Image 
-                        image={ImageP1} 
-                        title="Mastering UX/UI System Design In Figma"
-                        createdDate="April 14, 2024"
-                        level="Expert"
-                        duration="22h14m"
-                        price="180$"
-                    />
-                    <Image 
-                        image={ImageP2} 
-                        title="Mastering UX/UI System Design In Figma"
-                        createdDate="April 14, 2024"
-                        level="Intermediate"
-                        duration="15h30m"
-                        price="150$"
-                    />
-                    <Image 
-                        image={ImageP1} 
-                        title="Mastering UX/UI System Design In Figma"
-                        createdDate="April 14, 2024"
-                        level="Expert"
-                        duration="22h14m"
-                        price="180$"
-                    />
+                <div class="flex justify-evenly">
+                    <div className="grid grid-cols-3">
+                        <div className=" rounded-sm max-h-max">
+                            <Image
+                                image={ImageP1}
+                                title="Mastering UX/UI System Design In Figma"
+                                createdDate="April 14, 2024"
+                                level="Expert"
+                                duration="22h14m"
+                                price="180$"
+                            />
+                        </div>
+                        <div className="rounded-sm max-h-max">
+                        <Image
+                                image={ImageP1}
+                                title="Mastering UX/UI System Design In Figma"
+                                createdDate="April 14, 2024"
+                                level="Expert"
+                                duration="22h14m"
+                                price="180$"
+                            />
+                        </div>
+                        <div className="rounded-sm max-h-max">
+                            <Image
+                                image={ImageP1}
+                                title="Mastering UX/UI System Design In Figma"
+                                createdDate="April 14, 2024"
+                                level="Expert"
+                                duration="22h14m"
+                                price="180$"
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
     );
 }
-
-    	            
