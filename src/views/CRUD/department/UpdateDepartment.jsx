802e267167810afcde30267514090902ec18e869
noyrada <noyradastudentiq@gmail.com>
2024-11-05 12:59:20 +0700

    update and add route for updateDepartment

---
+++ src/views/CRUD/department/UpdateDepartment.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+// component 
+
+export default function UpdateDepartment() {
+    return (
+        <>
+            <div className="grid grid-rows-[auto,auto,1fr] mx-8 font-suwannaphum text-sm">
+                {/* Breadcrumb */}
+                <div className="text-md mb-3">
+                    <p>នាយកដ្ឋាន / កែប្រែ</p>
+                </div>
+
+                {/* Content Area with Cards */}
+                <div className="bg-white shadow-sm rounded-md mt-2 font-sans">
+                    <div >
+                       Updated
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
