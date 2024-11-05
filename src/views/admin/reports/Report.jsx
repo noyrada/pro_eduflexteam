d148fb914652212a945bcdc08a4ed21654a72ba0
noyrada <noyradastudentiq@gmail.com>
2024-11-05 22:25:32 +0700

    update sample for report

--- src/views/admin/reports/Report.jsx
+++ src/views/admin/reports/Report.jsx
@@ -3,14 +3,14 @@ import React from "react";
 
 // components
 import CardStats from "../../../components/Cards/CardStats";
-import CardLineChart from "../../../components/Cards/CardLineChart";
+import CardEventCourse from '../../../components/Cards/Courses/CardEvents'
 
 export default function Report() {
     return (
         <>
-            <div className="grid grid-cols-4 gap-5 mx-8 pt-0">
+            <div className="grid grid-rows-[auto,auto,1fr] mx-8">
                 {/* Card Stats */}
-                <div className="col-span-3   ">
+                <div className="">
                     <div class="grid grid-cols-4  gap-4">
                         <CardStats title="Course Completed" icon={<ion-icon name='document-outline'></ion-icon>} num="98" />
                         <CardStats title="In process" icon={<ion-icon name="trending-up-sharp"></ion-icon>} num="98" />
@@ -19,9 +19,13 @@ export default function Report() {
                     </div>
                 </div>
 
+                <div className="text-sm font-suwannaphum mt-4">
+                    <p>របាយការណ៍ / វគ្គសិក្សា</p>
+                </div>
+
                 {/* Card LineChart */}
-                <div className="col-span-3 bg-white shadow-sm rounded-sm h-100">
-                    <CardLineChart />
+                <div className="bg-white shadow-sm rounded-md mt-2 font-sans">
+                    <CardEventCourse/>
                 </div>
 
             </div>
