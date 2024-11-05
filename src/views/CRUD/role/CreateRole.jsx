e6838cc068c7e0f8a82fdd3872022af11c38aa88
noyrada <noyradastudentiq@gmail.com>
2024-11-05 12:48:05 +0700

    update and add route for updateRole

--- src/views/CRUD/role/CreateRole.jsx
+++ src/views/CRUD/role/CreateRole.jsx
@@ -1,26 +1,26 @@
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 import axios from 'axios';
 
 // components
 
 export default function CreateRole({ isOpen, onClose, onSubmit }) {
-    const [values,setValues]=useState({
-        RoleName:'',
-        Description:'',
-        TotalUser:''
+    const [values, setValues] = useState({
+        RoleName: '',
+        Description: '',
+        TotalUser: ''
     })
-    const navigate=useNavigate();
-    const handleSubmit=(event)=>{
-    event.preventDefault();
-    axios.post('http://localhost:3000/Roles',values)
-    .then(res => {
-      console.log(res);
-      navigate('/admin/users-role');
-      onClose();
-    })
-    .catch(err => console.log(err));
-  }
+    const navigate = useNavigate();
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('http://localhost:3000/Roles', values)
+            .then(res => {
+                console.log(res);
+                navigate('/admin/users-role');
+                onClose();
+            })
+            .catch(err => console.log(err));
+    }
     if (!isOpen) return null;
     return (
         <>
@@ -32,12 +32,12 @@ export default function CreateRole({ isOpen, onClose, onSubmit }) {
                             <label className="block text-gray-700 font-medium mb-1" htmlFor="roleName">
                                 Role Name
                             </label>
-                            <input 
-                                type="text" 
-                                name='roleName' 
-                                className="w-full p-2 border form-control border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500" 
-                                placeholder='Enter role name' 
-                                onChange={e=>setValues({...values,RoleName:e.target.value})}
+                            <input
+                                type="text"
+                                name='roleName'
+                                className="w-full p-2 border form-control border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                                placeholder='Enter role name'
+                                onChange={e => setValues({ ...values, RoleName: e.target.value })}
                                 required
                             />
                         </div>
@@ -45,35 +45,25 @@ export default function CreateRole({ isOpen, onClose, onSubmit }) {
                             <label className="block text-gray-700 font-medium mb-1" htmlFor="roleName">
                                 Description
                             </label>
-                            <input 
-                                type="text" 
-                                name='Description' 
-                                className="w-full p-2 border border-gray-300 form-control rounded focus:outline-none focus:ring-2 focus:ring-indigo-500" 
-                                placeholder='Enter description' 
-                                onChange={e=>setValues({...values,Description:e.target.value})}
+                            <input
+                                type="text"
+                                name='Description'
+                                className="w-full p-2 border border-gray-300 form-control rounded focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                                placeholder='Enter description'
+                                onChange={e => setValues({ ...values, Description: e.target.value })}
                                 required
                             />
                         </div>
-                        <div>
-                            <label className="block text-gray-700 font-medium mb-1" htmlFor="roleName">
-                                Total User
-                            </label>
-                            <input 
-                                type="text" 
-                                name='TotalUser' 
-                                className="w-full p-2 border form-control border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500" 
-                                placeholder='Enter total user' 
-                                onChange={e=>setValues({...values,TotalUser:e.target.value})}
-                                required
-                            />
-                        </div>
+                       
                         <div className="flex justify-end space-x-2">
-                            <button
-                                type="button"
-                                onClick={onClose} 
-                                className='bg-gray-300 text-gray-700 px-4 py-2 rounded hover:bg-gray-400 transition'>
-                                Cancel
-                            </button>
+                            <Link to="/admin/users-role">
+                                <button
+                                    type="button"
+                                    onClick={onClose}
+                                    className='bg-gray-300 text-gray-700 px-4 py-2 rounded hover:bg-gray-400 transition'>
+                                    Cancel
+                                </button>
+                            </Link>
 
                             <button
                                 type='submit'
