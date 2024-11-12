f0e99724c69daa5ce55388cb0afdf781be9cecfd
noyrada <noyradastudentiq@gmail.com>
2024-11-12 22:36:22 +0700

    updates users to khmers

--- src/views/CRUD/user/UpdateUser.jsx
+++ src/views/CRUD/user/UpdateUser.jsx
@@ -44,23 +44,22 @@ export default function UpdateUser() {
         {/* Content Area with Cards */}
         <div className="bg-white shadow-sm rounded-md mt-2 font-sans">
           <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-30 z-50 font-sans">
-            <div className="bg-white p-6 rounded-lg w-[40%] mx-4 shadow-lg h-[65%]">
-              <h3 className="text-lg font-semibold mb-2 text-center">
-                Update User
+            <div className="bg-white p-6 rounded-lg w-[40%] mx-4 shadow-lg h-[60%]">
+              <h3 className="text-md font-suwannaphum mb-2 text-center">
+                កែប្រែអ្នកប្រើប្រាស់
               </h3>
               <form onSubmit={handleUpdate} className="space-y-4">
                 <div>
                   <label
-                    className="block text-gray-700 font-medium mb-1"
+                    className="block text-gray-700 font-suwannaphum text-sm mb-1"
                     htmlFor="userName"
                   >
-                    User Name
+                    ឈ្មោះអ្នកប្រើប្រាស់
                   </label>
                   <input
                     type="text"
                     name="UserName"
-                    className="w-full p-2 border form-control border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                    placeholder="Enter user name"
+                    className="font-suwannaphum text-sm w-full p-2 border form-control border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500"
                     value={values.UserName}
                     onChange={(e) =>
                       setValues({ ...values, UserName: e.target.value })
@@ -70,16 +69,15 @@ export default function UpdateUser() {
                 </div>
                 <div>
                   <label
-                    className="block text-gray-700 font-medium mb-1"
+                    className="block text-gray-700 font-suwannaphum text-sm mb-1"
                     htmlFor="department"
                   >
-                    Department
+                    ដេប៉ាឌឺម៉ង់
                   </label>
                   <input
                     type="text"
                     name="Department"
                     className="w-full p-2 border border-gray-300 form-control rounded focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                    placeholder="Enter Department"
                     value={values.Department}
                     onChange={(e) =>
                       setValues({ ...values, Department: e.target.value })
@@ -89,16 +87,15 @@ export default function UpdateUser() {
                 </div>
                 <div>
                   <label
-                    className="block text-gray-700 font-medium mb-1"
+                    className="block text-gray-700 font-suwannaphum text-sm  mb-1"
                     htmlFor="roleName"
                   >
-                    Role Name
+                    តួនាទី
                   </label>
                   <input
                     type="text"
                     name="RoleName"
                     className="w-full p-2 border border-gray-300 form-control rounded focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                    placeholder="Enter Role"
                     value={values.RoleName}
                     onChange={(e) =>
                       setValues({ ...values, RoleName: e.target.value })
@@ -109,28 +106,26 @@ export default function UpdateUser() {
 
                 <div>
                   <label
-                    className="block text-gray-700 font-medium mb-1"
+                    className="block text-gray-700 font-suwannaphum text-sm  mb-1"
                     htmlFor="roleName"
                   >
-                    Status
+                    ស្ថានភាព
                   </label>
                   <input
                     type="text"
                     name="Status"
                     className="w-full p-2 border border-gray-300 form-control rounded focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                    placeholder="Enter Status"
                     value={values.Status}
                     onChange={(e) =>
                       setValues({ ...values, Status: e.target.value })
                     }
-                    required
                   />
                 </div>
 
-                <div className="flex justify-end space-x-2">
+                <div className="flex justify-end space-x-2 font-suwannaphum text-sm ">
                   <Link to={"/admin/users"}>
                     <button className="bg-gray-300 text-gray-700 px-4 py-2 rounded hover:bg-gray-400 transition">
-                      Cancel
+                      ថយក្រោយ
                     </button>
                   </Link>
 
@@ -138,7 +133,7 @@ export default function UpdateUser() {
                     type="submit"
                     className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 btn btn-success"
                   >
-                    Update
+                    កែប្រែ
                   </button>
                 </div>
               </form>
